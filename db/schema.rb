2517bccc40ad134931aeef0e7d20aa9072fdebb9
howdoicomputer <dr.frankinfurter@gmail.com>
2015-11-21 17:46:25 -0800

    feature/transition_to_postgres: finished transition to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151120184830) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "meetup_events", force: :cascade do |t|
     t.string   "name"
     t.string   "event_url"
